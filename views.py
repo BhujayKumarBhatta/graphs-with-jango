e0ae2617e4f7f6cbc413613e633b26497344b249
Bhujay Kumar Bhatta <bhujay.bhatta@yahoo.com>
2018-04-06 22:52:41 -0700

    date time date from view plotted , next adjust scale

--- views.py
+++ views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.shortcuts import render, render
+import datetime , time
 # Create your views here.
 def home(request):
     context = 'Hello Bhujay'
@@ -34,6 +35,52 @@ def rickshaw1(request):
             { 'x': 10, 'y': 38 }, 
             { 'x': 15, 'y': 30 }, 
             { 'x': 20, 'y': 56 } ]
-    context = {'ds1': ds1, 'msg': 'Hello Bhujay'}
+    
+    gd = [(datetime.datetime(2018, 3, 19, 9, 25), 300.0, 0.211498618533826), 
+           (datetime.datetime(2018, 3, 19, 9, 35), 300.0, 0.51186000010320402), 
+           (datetime.datetime(2018, 3, 19, 9, 45), 300.0, 2), 
+           (datetime.datetime(2018, 3, 19, 9, 55), 300.0, 14), 
+           (datetime.datetime(2018, 3, 19, 10, 5), 300.0, 30), 
+           (datetime.datetime(2018, 3, 19, 10, 15), 300.0, 40), 
+           (datetime.datetime(2018, 3, 19, 10, 25), 300.0, 50),
+           (datetime.datetime(2018, 3, 19, 10, 30), 300.0, 55), 
+           (datetime.datetime(2018, 3, 19, 10, 35), 300.0, 70), 
+           (datetime.datetime(2018, 3, 19, 10, 40), 300.0, 40), 
+           (datetime.datetime(2018, 3, 19, 10, 45), 300.0, 45), 
+           (datetime.datetime(2018, 3, 19, 10, 55), 300.0, 80),
+           (datetime.datetime(2018, 3, 19, 11, 00), 300.0, 82), 
+           (datetime.datetime(2018, 3, 19, 11, 05), 300.0, 85), 
+           (datetime.datetime(2018, 3, 19, 11, 10), 300.0, 90), 
+           (datetime.datetime(2018, 3, 19, 11, 15), 300.0, 100), 
+           (datetime.datetime(2018, 3, 19, 10, 20), 300.0, 100)
+           ]
+    ds2 = []
+    for i in gd:
+        a,b,c = i
+        dict = {'x': int(time.mktime(a.timetuple())), 'y': c}
+        ds2.append(dict)   
+    
+    context = {'ds1': ds1, 'ds2': ds2, 'msg': 'Hello Bhujay'}
     return render(request,'rickshaw1.html', context)
+
+
+#ds2 = [(datetime.datetime(2018, 3, 19, 9, 25, tzinfo=<FixedOffset u'+00:00' datetime.timedelta(0)>), 300.0, 0.211498618533826), (datetime.datetime(2018, 3, 19, 9, 35, tzinfo=<FixedOffset u'+00:00' datetime.timedelta(0)>), 300.0, 0.21186000010320402), (datetime.datetime(2018, 3, 19, 9, 45, tzinfo=<FixedOffset u'+00:00' datetime.timedelta(0)>), 300.0, 0.21243252666576), (datetime.datetime(2018, 3, 19, 9, 55, tzinfo=<FixedOffset u'+00:00' datetime.timedelta(0)>), 300.0, 0.211657972857297), (datetime.datetime(2018, 3, 19, 10, 5, tzinfo=<FixedOffset u'+00:00' datetime.timedelta(0)>), 300.0, 0.211205720023904), (datetime.datetime(2018, 3, 19, 10, 15, tzinfo=<FixedOffset u'+00:00' datetime.timedelta(0)>), 300.0, 0.211523038609912), (datetime.datetime(2018, 3, 19, 10, 25, tzinfo=<FixedOffset u'+00:00' datetime.timedelta(0)>), 300.0, 0.21524190015871103)]
+#     gd = [(datetime.datetime(2018, 3, 19, 9, 25), 300.0, 0.211498618533826), 
+#            (datetime.datetime(2018, 3, 19, 9, 35), 300.0, 0.51186000010320402), 
+#            (datetime.datetime(2018, 3, 19, 9, 45), 300.0, 2), 
+#            (datetime.datetime(2018, 3, 19, 9, 55), 300.0, 14), 
+#            (datetime.datetime(2018, 3, 19, 10, 5), 300.0, 30), 
+#            (datetime.datetime(2018, 3, 19, 10, 15), 300.0, 40), 
+#            (datetime.datetime(2018, 3, 19, 10, 25), 300.0, 50),
+#            (datetime.datetime(2018, 3, 19, 10, 30), 300.0, 55), 
+#            (datetime.datetime(2018, 3, 19, 10, 35), 300.0, 70), 
+#            (datetime.datetime(2018, 3, 19, 10, 40), 300.0, 40), 
+#            (datetime.datetime(2018, 3, 19, 10, 45), 300.0, 45), 
+#            (datetime.datetime(2018, 3, 19, 10, 55), 300.0, 80),
+#            (datetime.datetime(2018, 3, 19, 11, 00), 300.0, 82), 
+#            (datetime.datetime(2018, 3, 19, 11, 05), 300.0, 85), 
+#            (datetime.datetime(2018, 3, 19, 11, 10), 300.0, 90), 
+#            (datetime.datetime(2018, 3, 19, 11, 15), 300.0, 100), 
+#            (datetime.datetime(2018, 3, 19, 10, 20), 300.0, 100)
+#            ]
 
