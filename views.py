ae81631be3f1637615f45436757349567942d132
Bhujay Kumar Bhatta <bhujay.bhatta@yahoo.com>
2018-04-06 23:45:31 -0700

    scale automatically adjusts, amazing!!

--- views.py
+++ views.py
@@ -52,7 +52,10 @@ def rickshaw1(request):
            (datetime.datetime(2018, 3, 19, 11, 05), 300.0, 85), 
            (datetime.datetime(2018, 3, 19, 11, 10), 300.0, 90), 
            (datetime.datetime(2018, 3, 19, 11, 15), 300.0, 100), 
-           (datetime.datetime(2018, 3, 19, 10, 20), 300.0, 100)
+           (datetime.datetime(2018, 3, 19, 11, 20), 300.0, 100),
+           (datetime.datetime(2018, 3, 19, 11, 25), 300.0, 124),
+           (datetime.datetime(2018, 3, 19, 11, 30), 300.0, 130), 
+           (datetime.datetime(2018, 3, 19, 11, 35), 300.0, 140),
            ]
     ds2 = []
     for i in gd:
@@ -62,6 +65,21 @@ def rickshaw1(request):
     
     context = {'ds1': ds1, 'ds2': ds2, 'msg': 'Hello Bhujay'}
     return render(request,'rickshaw1.html', context)
+
+ 
+           
+            
+#            (datetime.datetime(2018, 3, 19, 11, 40), 300.0, 50),
+#            (datetime.datetime(2018, 3, 19, 11, 45), 300.0, 55), 
+#            (datetime.datetime(2018, 3, 19, 11, 50), 300.0, 70), 
+#            (datetime.datetime(2018, 3, 19, 11, 55), 300.0, 40), 
+#            (datetime.datetime(2018, 3, 19, 12, 00, 300.0, 45), 
+#            (datetime.datetime(2018, 3, 19, 13, 05), 300.0, 80),
+#            (datetime.datetime(2018, 3, 19, 13, 10), 300.0, 82), 
+#            (datetime.datetime(2018, 3, 19, 13, 15), 300.0, 85), 
+#            (datetime.datetime(2018, 3, 19, 13, 20), 300.0, 90), 
+#            (datetime.datetime(2018, 3, 19, 13, 25), 300.0, 100), 
+#            (datetime.datetime(2018, 3, 19, 13, 30), 300.0, 100))
 
 
 #ds2 = [(datetime.datetime(2018, 3, 19, 9, 25, tzinfo=<FixedOffset u'+00:00' datetime.timedelta(0)>), 300.0, 0.211498618533826), (datetime.datetime(2018, 3, 19, 9, 35, tzinfo=<FixedOffset u'+00:00' datetime.timedelta(0)>), 300.0, 0.21186000010320402), (datetime.datetime(2018, 3, 19, 9, 45, tzinfo=<FixedOffset u'+00:00' datetime.timedelta(0)>), 300.0, 0.21243252666576), (datetime.datetime(2018, 3, 19, 9, 55, tzinfo=<FixedOffset u'+00:00' datetime.timedelta(0)>), 300.0, 0.211657972857297), (datetime.datetime(2018, 3, 19, 10, 5, tzinfo=<FixedOffset u'+00:00' datetime.timedelta(0)>), 300.0, 0.211205720023904), (datetime.datetime(2018, 3, 19, 10, 15, tzinfo=<FixedOffset u'+00:00' datetime.timedelta(0)>), 300.0, 0.211523038609912), (datetime.datetime(2018, 3, 19, 10, 25, tzinfo=<FixedOffset u'+00:00' datetime.timedelta(0)>), 300.0, 0.21524190015871103)]
